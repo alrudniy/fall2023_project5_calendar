321804193df23707dea2d93427feb0406e87661b
Faith Ansah <fansah@drew.edu>
2023-12-04 02:45:47 -0500

    created buttons

--- Login.php
+++ Login.php
@@ -118,7 +118,7 @@ if(strlen(trim($usernameEmail))>1 && strlen(trim($h_pass))>1 )
 								<div class="row">
 									<div class="col-sm-12 col-xs-12">
 										<div class="mb-30">
-											<h3 class="text-center txt-dark mb-10" style ="font-family:Roboto Serif" style= "font-size:20px" > Login </h3>
+											<h3 class="text-center txt-dark mb-10" style ="font-family:Roboto Serif" style= "font-size:64px" > Login </h3>
 											
 										</div>
 
@@ -133,7 +133,8 @@ if(strlen(trim($usernameEmail))>1 && strlen(trim($h_pass))>1 )
 													<label class="pull-left control-label mb-10" for="exampleInputpwd_2"  style="font-family:Roboto Serif" style= "font-size:20px" >Password: </label>
 													<input type="password" class="form-control" required="" name="UserPass" id="exampleInputpwd_2">
                                                     <br/>
-                                                    <a class="capitalize-font txt-primary block mb-10 pull-right font-12" href="forgot-password.html">forgot password ?</a>
+                                                     <button type="button" onclick="alert('website coming soon')"> new user? register here! </button> 
+													 <button type="button" onclick="alert('website coming soon')">Guest? use without login </button>  
                                                     <div class="clearfix"></div>
 												</div>
 

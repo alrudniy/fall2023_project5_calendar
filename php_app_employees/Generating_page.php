c6acfc7c7f35a4429a87772b90fc3adc8f9360c7
Kaitlyn Sanchez <ksanchez@drew.edu>
2023-12-06 13:26:21 -0500

    new

--- php_app_employees/Generating_page.php
+++ php_app_employees/Generating_page.php
@@ -43,24 +43,29 @@ p{
 <div class="right">
   <h1><center>Generate your class schedule</center></h1>
     <form method="post" action="upload.php">
-        Class name: <input type="text" id="name" name="name" required><br><br/>
+        Class name: <input type="text" id="name" name="name" placeholder="CSCI 150" required><br><br/>
         Upload your current class schedule: <input type="file" id="uploaded_file" name="filename" 
                                                   accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" />><br><br/>
         Start date: <input type="date" id="start_date" name="start_date" required><br/><br/>
         End date: <input type="date" id="end_date" name="end_date" required><br/><br/>
-        When does class meet: <select id="cars" name="cars" size="5" multiple required>
+        When does class meet: <select id="meeting_date" name="meeting_date" size="5" multiple required>
           <option id="day.m" value="monday">Monday</option>
           <option id="day.t" value="tuesday">Tuesday</option>
           <option id="day.w" value="wednesday">Wednesday</option>
           <option id="day.r" value="thursday">Thursday</option>
           <option id="day.f" value="friday">Friday</option>
-        </select><br><br>
+        </select><br>
         <p> 
           <span class="special"> ** </span>
         Hold down the Ctrl (windows) / Command (Mac) button to select multiple options 
         <span class="special"> ** </span>
+        </p><br>
+        What dates do you not want to include? <input type="text" id="exception"name="exception" placeholder="mmddyyyy">
+        <p> 
+          <span class="special"> ** </span>
+        Separate each date with a comma following the format 
+        <span class="special"> ** </span>
         </p><br><br>
-        What dates do you not want to include? <input type="text" id="exception"name="exception"><br/><br/>
         <center><input type="submit" value="Submit" style="height:auto; width:auto;font-size:500px" > </center>
     </form>
 </div>

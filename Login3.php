4b8060f77e4eef9fc132cc3ed542daffdb06b62d
Faith Ansah <fansah@drew.edu>
2023-12-10 23:26:39 -0500

    changes

--- Login3.php
+++ Login3.php
@@ -50,7 +50,18 @@ if (isset($_POST["Submit_But"])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>Class Schedule Login</title>
-    <!-- Include your CSS files here -->
+    		<meta name="description" content="Payroll Login" />
+		<meta name="keywords" content="Payroll Login Page" />
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+		<!-- vector map CSS -->
+		<link href="vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+
+		<!-- Custom CSS -->
+		<link href="dist/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <!-- Preloader -->
